f89fe05a371928a0496a1b6636403f22a2e0ca90
Sushmitha-C27 <sushmithagowda0007@gmail.com>
2025-11-17 18:08:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -222,7 +222,7 @@ footer { visibility:hidden; }
 # -----------------------
 # Model selectors / name
 # -----------------------
-MODEL_NAME = "models/gemini-2.5-pro"
+MODEL_NAME = "models/gemini-1.5-flash"
 
 # -----------------------
 # Chatbot Function
